04f00ead93f3c233e19d8a478c611daaa894adb2
Anton Kesy <antonkesy@gmail.com>
2023-11-24 16:38:51 +0100

    format

--- src/Protobuf.hs
+++ src/Protobuf.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 module Protobuf (module Protobuf) where
 
 import Data.Word (Word32)
@@ -42,53 +43,84 @@ data FloatType
   | Float
   deriving (Show, Eq)
 
-data ScalarType = IntType IntType | FloatType FloatType | StringType | BytesType | BoolType
+data ScalarType
+  = IntType IntType
+  | FloatType FloatType
+  | StringType
+  | BytesType
+  | BoolType
   deriving (Show, Eq)
 
-data MapKey = StringKey String | IntKey IntType
+data MapKey
+  = StringKey String
+  | IntKey IntType
   deriving (Show, Eq)
 
-data MapValue = MapName String | ScalarType
+data MapValue
+  = MapName String
+  | ScalarType
   deriving (Show, Eq)
 
-data DataType = Scalar ScalarType | Compound Name | Map MapKey MapValue
+data DataType
+  = Scalar ScalarType
+  | Compound Name
+  | Map MapKey MapValue
   deriving (Show, Eq)
 
-data MessageField = MessageField DataType Name FieldNumber Repeat | MessageReserved MessageReservedValues
+data MessageField
+  = MessageField DataType Name FieldNumber Repeat
+  | MessageReserved MessageReservedValues
   deriving (Show, Eq)
 
-data Message = Message MessageName [MessageField]
+data Message
+  = Message MessageName [MessageField]
   deriving (Show, Eq)
 
-data ReservedNames = ReservedNames [Name]
+data ReservedNames where
+  ReservedNames :: [Name] -> ReservedNames
   deriving (Show, Eq)
 
-data MessageReservedValues = ReservedMessageNumbers [FieldNumber] | ReservedMessageNames ReservedNames
+data MessageReservedValues
+  = ReservedMessageNumbers [FieldNumber]
+  | ReservedMessageNames ReservedNames
   deriving (Show, Eq)
 
 -- TODO: make reserved type generic
-data EnumReservedValues = ReservedEnumNumbers [EnumNumber] | ReservedEnumNames ReservedNames
+data EnumReservedValues
+  = ReservedEnumNumbers [EnumNumber]
+  | ReservedEnumNames ReservedNames
   deriving (Show, Eq)
 
-data EnumField = EnumValue Name EnumNumber | EnumOption Name Bool | EnumReserved EnumReservedValues
+data EnumField
+  = EnumValue Name EnumNumber
+  | EnumOption Name Bool
+  | EnumReserved EnumReservedValues
   deriving (Show, Eq)
 
-data Enum = Enum EnumName [EnumField]
+data Enum
+  = Enum EnumName [EnumField]
   deriving (Show, Eq)
 
-data Service = Service Name [RPC]
+data Service
+  = Service Name [RPC]
   deriving (Show, Eq)
 
-data RequestType = RequestType MessageName | RequestTypeStream MessageName
+data RequestType
+  = RequestType MessageName
+  | RequestTypeStream MessageName
   deriving (Show, Eq)
 
-data ReplyType = ReplyType MessageName | ReplyTypeStream MessageName
+data ReplyType
+  = ReplyType MessageName
+  | ReplyTypeStream MessageName
   deriving (Show, Eq)
 
-data RPC = RPC RPCName RequestType ReplyType
+data RPC
+  = RPC RPCName RequestType ReplyType
   deriving (Show, Eq)
 
-data Option = Option Name Value
+data Option
+  = Option Name Value
   deriving (Show, Eq)
 
 data Protobuf = Protobuf
