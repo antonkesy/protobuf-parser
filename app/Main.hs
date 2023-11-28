f69f0e79f2774aaa1913deb7d686ab0062c81b06
Anton Kesy <antonkesy@gmail.com>
2023-11-28 20:17:05 +0100

    create CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Options.Applicative
 import Prettyprinter
   ( Pretty (pretty),
     defaultLayoutOptions,
@@ -7,28 +8,45 @@ import Prettyprinter
   )
 import Prettyprinter.Render.String (renderString)
 import Text.Protobuf.Parser
+import Text.Protobuf.Types
 
--- import ProtoParser.Message
--- import Text.Parsec (parse)
+data Options = Options (Maybe FilePath) Bool [String]
 
--- testMessageReserved :: String
--- testMessageReserved =
---   "message Foo {\
---   \int32 foo = 1 [default = true];\
---   \}"
-
--- main :: IO ()
--- main = do
---   case parse parseMessage "" testMessageReserved of
---     Left err -> print err
---     Right protobuf -> print protobuf
+parseOptions :: Parser Options
+parseOptions =
+  Options
+    <$> optional
+      ( strOption
+          (long "file" <> short 'f' <> metavar "PATH" <> help "Specify file path to parse")
+      )
+    <*> switch (long "pretty" <> short 'p' <> help "Enable pretty print")
+    <*> many (argument str (metavar "STRING..."))
 
 main :: IO ()
 main = do
-  result <- parseProtoFile "test/E2E/protofiles/chat.proto"
+  opts <- execParser $ info (parseOptions <**> helper) fullDesc
+  processOptions opts
+
+processOptions :: Options -> IO ()
+processOptions (Options Nothing False []) =
+  putStrLn "Arguments: No file path provided or strings provided"
+processOptions (Options (Just path) isPrettier []) = do
+  result <- parseProtoFile path
   case result of
     Left err -> putStrLn $ "Parse error: " ++ show err
     Right protobuf ->
+      protoPrint protobuf isPrettier
+processOptions (Options _ isPrettier otherArgs) =
+  case parseProtobuf (unwords otherArgs) of
+    Left err -> putStrLn $ "Parse error: " ++ show err
+    Right protobuf ->
+      protoPrint protobuf isPrettier
+
+protoPrint :: Protobuf -> Bool -> IO ()
+protoPrint protobuf isPrettier =
+  if not isPrettier
+    then putStrLn $ show protobuf
+    else
       putStrLn $
         renderString $
           layoutPretty defaultLayoutOptions $
